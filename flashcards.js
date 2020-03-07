ffbbb831cd8da5127a267bc17f79b552dea1448d
bouncymist <60857815+bouncymist@users.noreply.github.com>
2020-03-07 21:53:54 +0000

    Update flashcards.js

--- flashcards.js
+++ flashcards.js
@@ -27,16 +27,16 @@ var Question_list = [
 	'Why was Islamic medicine generally more rational and evidence-based than European medicine?',
 	
 	];
-var Answer_list = [
+var answer2list =[
 	'Miasma (bad air) or Four humours out of balance',
 	'Punishment from God or Demons/witches or Evil spirits or Astrology',
-	'Hippocrates (c460 – c377 BC)',
+	'Hippocrates',
 	'Galen',
 	'Blood, phlegm, yellow bile and black bile',
 	'The Christian Church',
 	'His ideas were based mainly from his dissection of animals.',
-	'A promise that they will follow certain rules when treating patients, such as ‘do no harm’.',
-	'Barber surgeons or Wise men/women or Travelling healers or Herbalists in monasteries or Trained doctors or an Apothecary',
+	'A promise that they will follow certain rules when treating patients, such as "do no harm".',
+	'Barber surgeons, Wise men/women, Travelling healers, Herbalists in monasteries, Trained doctors, an Apothecary',
 	'7',
 	'Read ancient texts like those of Hippocrates and Galen, Read writings from the Islamic world and Very little, if any, practical experience',
 	'A person who prepared and sold remedies such as herbals',
@@ -55,16 +55,165 @@ var Answer_list = [
 	'By Islamic doctors',
 	'Partly due to their knowledge of classical texts such the work of Hippocrates and Galen'
 	
-	];
+]
+	
+var answer_list = [
+{
+	anslist: answer2list},
+	
+	{
+		anslist: [
+	'God, The alignment of the stars',
+	'Miasma (bad air), four humours out of balance',
+	'Galen',
+	'Hippocrates',
+	'Spit, Sick, Juice and Brain fluid',
+	'Islam',
+	'He was blind!',
+	'A promise that they will follow certain rules such as "do not kill anyone else".',
+	'A teacher, a parent, your friend, any responsible adult, a carer',
+	'2',
+	'Practice on dead bodies.',
+	'A person who carried out surgery.',
+	'1',
+	'1',
+	'1',
+	'1',
+	'1',
+	'1',
+	'1',
+	'1',
+	'1',
+	'1',
+	'1',
+	'1',
+	'1',
+	'1'
+
+	]},
+	
+	{
+	anslist: [
+	'Germs',
+	'Werewolves',
+	'Pythagoras',
+	'Julius Caesar',
+	'Dark, Light, Toilet and Cool',
+	'Jews',
+	'Writing hadnt been invented yet',
+	'An ancient greek recipe for bread.',
+	'A pharmacist, a local GP, a paediatrician, a dentist, a wise friend. ',
+	'6',
+	'An apprenticeship with an experienced doctor.',
+	'A person who carried out surgery.',
+	'2',
+	'2',
+	'2',
+	'2',
+	'2',
+	'2',
+	'2',
+	'2',
+	'2',
+	'2',
+	'2',
+	'2',
+	'2',
+	'2'
+
+	]},
+
+	
+	{ 
+		anslist: [
+	'A plant invasion',
+	'Angry spirits',
+	'Jesus',
+	'The Pope',
+	'Happy, Sad, Angry, Disgusted',
+	'Hindus',
+	'He dissected only deformed humans',
+	'An agreement to go to Hippocrates grave one day.',
+	'Google, Bing, your Insta DMs, an online poll, yourself.',
+	'As many as they liked',
+	'Watching 24 hours in A&E on repeat',
+	'A person who sold only fake medicines',
+	'3',
+	'3',
+	'3',
+	'3',
+	'3',
+	'3',
+	'3',
+	'3',
+	'3',
+	'3',
+	'3',
+	'3',
+	'3',
+	'3'
+
+	]},
+]
+	
 var qnumber = 0;
 var quest = Question_list[qnumber];
-var ans = Answer_list[qnumber];
+var ans = answer2list[qnumber];
+var ans_position = 0;
+
+function shuffleArray(array) {
+    for (var i = array.length - 1; i > 0; i--) {
+        var j = Math.floor(Math.random() * (i + 1));
+        var temp = array[i];
+        array[i] = array[j];
+		array[j] = temp;
+	}
+}
+
 function showq() {
 			qnumber = Math.floor(Math.random() * Question_list.length);
 			document.getElementById('Question').innerHTML = Question_list[qnumber];
-			document.getElementById('Answer').innerHTML = 'Click here for answer';
+			shuffleArray(answer_list);
+			document.getElementById('1').innerHTML = answer_list[0].anslist[qnumber];
+			document.getElementById('2').innerHTML = answer_list[1].anslist[qnumber];
+			document.getElementById('3').innerHTML = answer_list[2].anslist[qnumber];
+			document.getElementById('4').innerHTML = answer_list[3].anslist[qnumber];
+			document.getElementById('1').style.background = '#CCF6FF';
+			document.getElementById('2').style.background ='#CCF6FF';
+			document.getElementById('3').style.background = '#CCF6FF';
+			document.getElementById('4').style.background = '#CCF6FF';
+			
 			}
-function showa() {
-	document.getElementById('Answer').innerHTML = Answer_list[qnumber];
+			
+function showa1() {
+	if (answer2list[qnumber] == document.getElementById('1').innerHTML){
+		document.getElementById('1').style.background = '#7fff78';
+	}else {
+		document.getElementById('1').style.background = '#ff8282';
+	}
+	
+		}
+function showa2() {
+	if (answer2list[qnumber] == document.getElementById('2').innerHTML){
+		document.getElementById('2').style.background = '#7fff78';
+	}else{
+		document.getElementById('2').style.background = '#ff8282';
+	}
+	
+		}
+function showa3() {
+	if (answer2list[qnumber] == document.getElementById('3').innerHTML){
+		document.getElementById('3').style.background = '#7fff78';
+	}else{
+		document.getElementById('3').style.background = '#ff8282';
+	}
+	
+}
+function showa4() {
+	if (answer2list[qnumber] == document.getElementById('4').innerHTML){
+		document.getElementById('4').style.background = '#7fff78';
+	}else{
+		document.getElementById('4').style.background = '#ff8282';
+	}
 	
 		}
